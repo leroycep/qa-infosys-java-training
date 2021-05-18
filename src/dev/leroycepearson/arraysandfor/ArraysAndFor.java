5fbd27f52f90de9bc671f31740492f058a238109
LeRoyce Pearson <leroycepearson@geemili.xyz>
2021-05-18 07:40:21 -0600

    Complete arrays exercise 1

---
+++ src/dev/leroycepearson/arraysandfor/ArraysAndFor.java
@@ -0,0 +1,13 @@
+package dev.leroycepearson.arraysandfor;
+
+class ArraysAndFor {
+
+    public static void main(String[] args) {
+        int[] values = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
+
+        for (int val: values) {
+            System.out.println(val);
+        }
+    }
+
+}
